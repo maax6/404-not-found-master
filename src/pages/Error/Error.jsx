5c1356516d193c950a6fad0a5d3d22f349887804
Maxime <maxsauzeat@gmail.com>
2023-02-11 14:27:21 +0100

    Revert "Tentative fix page highness"
    This reverts commit c6c4dd3c1ecdea7a8f0e9c828726c5d012f851e2.

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,15 +2,15 @@ import scarecrow from './Scarecrow.png'
 
 export default function Error() {
   return (
-    <div className="wrapper-404">
-      <header className="notFound-logo">
+    <main>
+      <header className="notFound">
         <strong>404 NOT FOUND</strong>
       </header>
-      <div id="switchFlex">
+      <div id="flex">
         <div className="scarecrow">
           <img className="scarecrow__img" src={scarecrow} alt="épouvantail" />
         </div>
-        <div className="sec">
+        <section className="sec">
           <h1 id="badNews"> I have bad news for you</h1>
           <p className="sec__p">
             The page you are looking for might be removed or is temporarily
@@ -19,11 +19,11 @@ export default function Error() {
           <a href="/" className="return">
             <span className="return__txt">BACK TO HOMEPAGE</span>
           </a>
-        </div>
+        </section>
       </div>
       <footer>
         <p>created by ⌘ Maxime - devChallenges.io</p>
       </footer>
-    </div>
+    </main>
   )
 }
