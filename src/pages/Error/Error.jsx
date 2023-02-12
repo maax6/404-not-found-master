d296b9ed0843302a67740b83b7afdbf68e194a01
Maxime <85428733+maax6@users.noreply.github.com>
2023-02-12 21:49:37 +0100

    add containers 🤓

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -6,24 +6,31 @@ export default function Error() {
       <header className="notFound">
         <strong>404 NOT FOUND</strong>
       </header>
-      <div className="flexContainer">
-        <div className="scarecrow">
-          <img className="scarecrow__img" src={scarecrow} alt="épouvantail" />
-        </div>
-        <section className="badNews">
-          <div className="badNews__texts">
-            <h1 className="badNews__texts__title"> I have bad news for you</h1>
-            <p className="badNews__texts__speech">
-              The page you are looking for might be removed or is temporarily
-              unavailable
-            </p>
+      
+        <div className="flexContainer">
+          <div className="panoramic">
+          <div className="scarecrow">
+            <img className="scarecrow__img" src={scarecrow} alt="épouvantail" />
           </div>
-          <a href="/" className="badNews__btn">
-            <span className="badNews__btn__txt">BACK TO HOMEPAGE</span>
-          </a>
-        </section>
-      </div>
-      <p className="text-footer">created by ⌘ Maxime - devChallenges.io</p>
+          <section className="badNews">
+            <div className="badNews__texts">
+              <h1 className="badNews__texts__title">
+                {' '}
+                I have bad news for you
+              </h1>
+              <p className="badNews__texts__speech">
+                The page you are looking for might be removed or is temporarily
+                unavailable
+              </p>
+            </div>
+            <a href="/" className="badNews__btn">
+              <span className="badNews__btn__txt">BACK TO HOMEPAGE</span>
+            </a>
+          </section>
+          </div>
+        </div>
+        <p className="text-footer">created by ⌘ Maxime - devChallenges.io</p>
+      
     </div>
   )
 }
