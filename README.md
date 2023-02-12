40edcd6f341773fd26f62e5fa946ef0c9492f1b4
Maxime <maxsauzeat@gmail.com>
2023-02-12 17:38:37 +0100

    small refacto flex
    bem, nesting sass

--- README.md
+++ README.md
@@ -8,11 +8,11 @@
 
 <div align="center">
   <h3>
-    <a href="https://{your-demo-link.your-domain}">
+    <a href="https://404-not-found-master-seven.vercel.app/">
       Demo
     </a>
     <span> | </span>
-    <a href="https://{your-url-to-the-solution}">
+    <a href="https://github.com/maax6/404-not-found-master">
       Solution
     </a>
     <span> | </span>
--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,22 +2,24 @@ import scarecrow from './Scarecrow.png'
 
 export default function Error() {
   return (
-    <div className='wrapper-404'>
+    <div className="wrapper-404">
       <header className="notFound">
         <strong>404 NOT FOUND</strong>
       </header>
-      <div id="flex">
+      <div className="flexContainer">
         <div className="scarecrow">
           <img className="scarecrow__img" src={scarecrow} alt="épouvantail" />
         </div>
-        <section className="sec">
-          <h1 id="badNews"> I have bad news for you</h1>
-          <p className="sec__p">
-            The page you are looking for might be removed or is temporarily
-            unavailable
-          </p>
-          <a href="/" className="return__btn">
-            <span className="return__btn__txt">BACK TO HOMEPAGE</span>
+        <section className="badNews">
+          <div className="badNews__texts">
+            <h1 className="badNews__texts__title"> I have bad news for you</h1>
+            <p className="badNews__texts__speech">
+              The page you are looking for might be removed or is temporarily
+              unavailable
+            </p>
+          </div>
+          <a href="/" className="badNews__btn">
+            <span className="badNews__btn__txt">BACK TO HOMEPAGE</span>
           </a>
         </section>
       </div>
